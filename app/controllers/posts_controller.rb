aede7ed6926f5c2ef8ab19068b93e8bc25e82561
Edem <edem228@gmail.com>
2016-01-07 21:22:58 +0100

    refactoring posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,6 +57,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :description, :user_id, :image)
+    params.require(:post).permit(:title, :description, :image)
   end
 end
